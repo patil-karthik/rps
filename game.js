b381f23d9ea7d195b58619c218a554ed79b5318b
Karthik <karthikgoudampatil07@gmail.com>
2022-03-31 17:27:41 +0530

    gane

---
+++ game.js
@@ -0,0 +1,5 @@
+const rockbtn=document.querySelector(`.rock-btn`)
+const paperbtn=document.querySelector(`.paper-btn`)
+const sissorbtn=document.querySelector(`.sissor-btn`)
+
+const secretnum =math.trunc(math.rondom()*3)+1
